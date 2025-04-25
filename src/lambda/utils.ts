5e2c88ef6c8ff9aa80cf63ecc47ae9e7f846ea8d
Boris Serdiuk <just-boris@hotmail.com>
2025-04-25 08:13:17 +0200

    Revert "happy easter"
    This reverts commit af0a30680c2e03d1990293a1c7d5b5a60d5ae625.

--- src/lambda/utils.ts
+++ src/lambda/utils.ts
@@ -12,15 +12,8 @@ export function errorToString(error: unknown) {
 }
 
 export function formatMessage(message: string) {
-  let result = message.replace(/%(\S+)%/g, (_, exp) => {
+  return message.replace(/%(\S+)%/g, (_, exp) => {
     const date = applyDuration(new Date(), exp);
     return `${date.getDate()}.${(date.getMonth() + 1).toString().padStart(2, "0")}`;
   });
-
-  if (result.includes("20.04")) {
-    // special Easter schedule
-    result = result.replace("20.04", "21.04").replace("воскресенье", "понедельник");
-  }
-
-  return result;
 }
